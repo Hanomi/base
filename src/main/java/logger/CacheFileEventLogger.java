655ba360599ce7220cc987e13ef1a929df240f3f
Hanomi <freefpdie@gmail.com>
2018-04-27 19:03:08 +0700

    split beans xml

--- src/main/java/logger/CacheFileEventLogger.java
+++ src/main/java/logger/CacheFileEventLogger.java
@@ -5,11 +5,12 @@ import java.util.List;
 
 public class CacheFileEventLogger extends FileEventLogger {
     private int cacheSize;
-    private List<Event> cache = new ArrayList<>();
+    private List<Event> cache;
 
     public CacheFileEventLogger(int cacheSize, String filename) {
         super(filename);
         this.cacheSize = cacheSize;
+        this.cache = new ArrayList<>(cacheSize);
     }
 
     @Override
