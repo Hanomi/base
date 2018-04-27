2a786be7509a65c81362619802f07a19a32bc19b
Hanomi <freefpdie@gmail.com>
2018-04-27 19:16:05 +0700

    property

--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -3,6 +3,7 @@ package data;
 public class Client {
     private int id;
     private String fullName;
+    private String greeting;
 
     public Client(int id, String fullName) {
         this.id = id;
@@ -13,15 +14,11 @@ public class Client {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getFullName() {
         return fullName;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
     }
 }
